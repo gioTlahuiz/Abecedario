8d59c0eb813fd19816c39963b591ebbf160468ff
EMGA <erickmgarciaa@outlook.com>
2026-03-12 15:33:37 -0600

    se agrego style y js

---
+++ script.js
@@ -0,0 +1,11 @@
+function setRandomColor(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    // creamos una cadena que contiene la coloracion rgb
+    const color = `rgb(${r},${g},${b})`;
+
+    // aqui debemos llamar al elemento que le dimos el id random
+    document.getElementById("random").style.color = color;
+}
